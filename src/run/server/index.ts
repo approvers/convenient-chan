bcfa950b3e944b3ef3eb2f44406a0c2df4285222
merunno <merunno92@gmail.com>
2022-02-06 04:36:15 +0900

    First commit

---
+++ src/run/server/index.ts
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+import { Client, ClientUser } from 'discord.js';
+import console from 'console';
+
+dotenv.config();
+const token = process.env.DISCORD_TOKEN;
+if (!token) {
+  console.error('環境変数が指定されてないよ。');
+  process.exit(1);
+}
+
+const client = new Client({
+  intents: ['GUILDS', 'GUILD_MESSAGES']
+});
+
+void client.login(token);
+
+client.on('ready', () => {
+  const shardClient: ClientUser | null = client.user;
+  const shardClientV: string = process.env.npm_package_version ?? '不明';
+  if (!shardClient) return;
+
+  console.log(`${shardClient.username}@${shardClientV} 起動しています......`);
+
+  // アクティビティ登録
+
+  setInterval(function () {
+    shardClient.setActivity(
+      `v${shardClientV} ｜ /help ｜ Ping: ${client.ws.ping}ms`
+    );
+  }, 1000 * 15);
+});
